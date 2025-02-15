3564d2dcc25724048929b004fc38a04242ca55e2
Nini-Pheradze <peradzenino51@gmail.com>
2025-02-15 08:39:07 -0800

    commit

--- Level 38/classwork/lesson38.py
+++ Level 38/classwork/lesson38.py
@@ -4,7 +4,7 @@ my_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 for index in range(len(my_tuple)):
     print(f"Index {index}: {my_tuple[index]}")
 
-
+#შექმენით ფუნქცია სახელად no_duplicates, რომელსაც გადაეცემა ერთი პარამეტრი - user_list. user_list-ის მონაცემთა ტიპია სია, ხოლო თქვენი დავალებაა რომ ფუნქციამ დააბრუნოს ეს სია იმ სახით, რომ მასში დუპლიკატები არ გვქონდეს. ფუნქცია გამოიძახეთ 3-ჯერ, განსხვავებული არგუმენტებით
 def no_duplicates(user_list):
     return list(set(user_list))
 
