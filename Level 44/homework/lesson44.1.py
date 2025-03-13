28353ac9ad1ff3e5d3766dfb664fdf2bb91c7a61
Nini-Pheradze <peradzenino51@gmail.com>
2025-03-13 23:29:28 +0400

    commit

--- Level 44/homework/lesson44.1.py
+++ Level 44/homework/lesson44.1.py
@@ -2,6 +2,7 @@
 def reverse_seq(n):
     return [i for i in range(n, 0, -1)]
 
+
 # If you can't sleep, just count sheep!!
 def count_sheep(n):
     return ''.join(f"{i} sheep..." for i in range(1, n + 1))
@@ -22,3 +23,16 @@ def get_grade(s1, s2, s3):
         return 'D'
     else:
         return 'F'
+
+
+# Is n divisible by x and y?
+def is_divisible(n,x,y):
+    return n % x == 0 and n % y == 0
+
+
+# If you can't sleep, just count sheep!!
+def count_sheep(n):
+    return ''.join(f"{i} sheep..." for i in range(1, n+1))
+
+
+# # Rock Paper Scissors! ???
