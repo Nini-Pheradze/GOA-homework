158ddf3f0f472d08ef702ce12d39a7a67f24bfd9
Nini-Pheradze <peradzenino51@gmail.com>
2025-04-12 23:54:15 +0400

    commit

--- Level 54/classwork/lesson54.py
+++ Level 54/classwork/lesson54.py
@@ -10,6 +10,20 @@
 
 # Regardless of what happens, use a finally block to print a message like “Operation complete."
 
+num1 = float(input("Enter numerator: "))
+num2 = float(input("Enter denominator: "))
+
+try:
+    print(num1/num2)
+except ValueError:
+    raise ValueError("Denominator cannot be equel to zero")
+except:
+    print("invalid input")
+finally:
+    print("Operation is completed")
+
+
+
 
 #Write a function apply_to_list(func, values) that takes a function func and a list values, and returns a new list where func is applied to each element.Then: Define a function square(x) that returns the square of a number.
 
