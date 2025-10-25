62949bc75b2b6a8723dacf86005910259ce1cdd3
Nini-Pheradze <peradzenino51@gmail.com>
2025-10-25 22:48:52 +0400

    commit

---
+++ Level 113/classwork/lesson113.js
@@ -0,0 +1,10 @@
+// prompt-ით მომხმარებელს შემოატანინეთ ნებისმიერი სიტყვა და ეს სიტყვა შეინახეთ localStorage-ში.
+
+// შემდეგ, localStorage-დან წამოიღეთ ეს სიტყვა და ის textContent-ად გაუწერეთ პარაგრაფს. 
+
+// როდესაც ვებსაიტი დარეფრეშდება და localStorage-ში ინფორმაცია უკვე გექნებათ შენახული, პარაგრაფის textContent უნდა იყოს localStorage-ში ბოლოს შენახული
+
+let userInput = prompt("Enter a word: ");
+
+parent.textcontent = localStorage.se
+
