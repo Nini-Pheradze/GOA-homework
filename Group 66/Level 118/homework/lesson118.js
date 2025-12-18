c0f1720f082386166681279defa5519ae4202341
Nini-Pheradze <peradzenino51@gmail.com>
2025-12-19 01:18:30 +0400

    'commit'

---
+++ Group 66/Level 118/homework/lesson118.js
@@ -0,0 +1,24 @@
+// HTTP stands for Hyper Text Transfer Protocol
+
+// WWW is about communication between web clients and servers
+
+// Communication between client computers and web servers is done by sending HTTP Requests and receiving HTTP Responses
+
+
+// Communication between clients and servers is done by requests and responses:
+
+// A client (a browser) sends an HTTP request to the web
+// A web server receives the request
+// The server runs an application to process the request
+// The server returns an HTTP response (output) to the browser
+// The client (the browser) receives the response
+
+
+
+// A typical HTTP request / response circle:
+
+// The browser requests an HTML page. The server returns an HTML file.
+// The browser requests a style sheet. The server returns a CSS file.
+// The browser requests an JPG image. The server returns a JPG file.
+// The browser requests JavaScript code. The server returns a JS file
+// The browser requests data. The server returns data (in XML or JSON).
